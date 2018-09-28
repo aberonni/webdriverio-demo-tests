b7433367533d68b0df249b3e9721b96672da77f0
Domenico Gemoli <domenicogemoli@gmail.com>
2018-09-29 00:35:53 +0200

    Add debug configuration

---
+++ wdio.conf.debug.js
@@ -0,0 +1,14 @@
+const { config } = require('./wdio.conf');
+
+// This configuration is used by vscode for debugging
+exports.config = {
+    ...config,
+    maxInstances: 1,
+    execArgv: ['--inspect-brk=127.0.0.1:5859'],
+    capabilities: [
+        {
+            maxInstances: 1,
+            browserName: 'chrome',
+        },
+    ],
+};
