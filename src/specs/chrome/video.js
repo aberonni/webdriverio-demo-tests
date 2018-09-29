9ea3566f1390d3fcd70287668515f87e3bee02c9
Domenico Gemoli <domenicogemoli@gmail.com>
2018-09-29 03:49:58 +0200

    better video test

--- src/specs/chrome/video.js
+++ src/specs/chrome/video.js
@@ -1,13 +1,20 @@
-describe('Check video on a website', function() {
-    it('should have paused video on landing, and start playing when clicking on it', function() {
+describe('Check jwplayer demo page', function() {
+    it('should autoplay video, and allow for pausing and playing', function() {
         browser.url(
-            'http://techslides.com/sample-webm-ogg-and-mp4-video-files-for-html5'
+            'https://developer.jwplayer.com/jw-player/demos/basic/chromeless-player/'
         );
 
-        const videoSelector = 'video';
+        browser.timeouts('script', 60000);
+
+        const videoSelector = '.jw-video';
+        const pauseButtonSelector = '#pause';
+        const playButtonSelector = '#play';
+
         browser.waitForExist(videoSelector, 10000);
+        browser.assertVideoPlaying(videoSelector);
+        browser.click(pauseButtonSelector);
         browser.assertVideoPaused(videoSelector);
-        browser.click(videoSelector);
+        browser.click(playButtonSelector);
         browser.assertVideoPlaying(videoSelector);
     });
 });
