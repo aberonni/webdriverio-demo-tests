813f906262a735b70b54e076cfc88c84c0011aaa
Domenico Gemoli <domenicogemoli@gmail.com>
2018-10-27 16:25:32 +0200

    fix zalenium versions

--- start-zalenium.sh
+++ start-zalenium.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-docker pull elgalu/selenium
+docker pull elgalu/selenium:3.14.0-p13
 
-docker pull dosel/zalenium
+docker pull dosel/zalenium:3.14.0f
 
 docker run --rm -ti --name zalenium -p 4444:4444 \
       -v /var/run/docker.sock:/var/run/docker.sock \
